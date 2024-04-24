a2b25a7f03f0f85cea83ab2708b29f094696ebc9
mikktur <mikko.silenceri@gmail.com>
2024-04-24 12:24:30 +0300

    login should be functional now

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,9 +4,9 @@ import {registerUser, login} from './api.js';
 //Register user post request
 regForm.addEventListener('submit', async (evt) => {
   evt.preventDefault();
-  registerUser(regForm);
+  await registerUser(regForm);
 });
 loginForm.addEventListener('submit', async (e) => {
-  e.preventDefault;
-  login(loginForm);
+  e.preventDefault();
+  await login(loginForm);
 });
