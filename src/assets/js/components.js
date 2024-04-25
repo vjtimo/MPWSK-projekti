a3e1a61a4528a47337adfd4074deb83ea66a1757
mikktur <mikko.silenceri@gmail.com>
2024-04-25 17:26:16 +0300

    fixed the layout of menu components

--- src/assets/js/components.js
+++ src/assets/js/components.js
@@ -1,1 +1,36 @@
+import {fetchData} from './api.js';
+const displayItems = async () => {
+  try {
+    const data = await fetchData('api/pizzas');
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+      const name = data[i].nimi;
+      const description = data[i].kuvaus_fi;
+      const price = data[i].hinta;
+      const div = document.querySelector('#pizzat');
 
+      const html = `
+      <a href="menu.html" class="link">
+      <figure>
+        <img src="pictures/pizza.jpg" alt="Pizza" />
+
+        <figcaption>
+          <div class="figure-header">
+            <h3>${name}</h3>
+            <p>${price}€</p>
+          </div>
+          <p>
+            ${description}
+          </p>
+        </figcaption>
+      </figure>
+    </a>
+    `;
+
+      div.innerHTML += html;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export {displayItems};
